2a387bdd8dfcc76bfb1619d06b73a7b354973740
sonu parihar <sparihar729@gmail.com>
2021-11-07 10:53:35 +0530

    Ensuring the player gets exact winning position

--- src/day_4/SnakeLadder.java
+++ src/day_4/SnakeLadder.java
@@ -1,19 +1,20 @@
 package day_4;
 
 public class SnakeLadder {
-    
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
+    public static final int LOWER_LIMIT = 0;
+    public static final int UPPER_LIMIT = 100;
 
     public static void main(String[] args)
     {
         int position = 0;
         int dieCheck;
         int optionCheck;
-        while(position<100)
+        while(position<UPPER_LIMIT)
         {
-            dieCheck = ((int) ((Math.random())*10)%6)+1;
+            dieCheck = ((int)((Math.random())*10)%6)+1;
             optionCheck =(int) ((Math.random())*10)%3;
             switch(optionCheck)
             {
@@ -21,16 +22,31 @@ public class SnakeLadder {
                     position=position;
                     break;
                 case LADDER:
-                    position=position+dieCheck;
+                    if((position+dieCheck)>UPPER_LIMIT)
+                    {
+                        position=position;
+                    }
+                    else
+                    {
+                        position=position+dieCheck;
+                    }
                     break;
+
                 default:
-                    position=position-dieCheck;
-                    if(position<=0)
+
+                    if((position-dieCheck)<=LOWER_LIMIT)
                     {
                         position=0;
                     }
+                    else
+                    {
+                        position=position-dieCheck;
+                    }
+                    break;
             }
+            System.out.println("Dice Number: " + dieCheck);
+            System.out.println("Option :" + optionCheck);
+            System.out.println("The Position is: " + position);
         }
-        System.out.println("The Position is: "+position);
     }
 }

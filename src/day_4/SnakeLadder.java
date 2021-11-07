ef7992346f3f778e8cab8a4e10190366bfcd7b9d
sonu parihar <sparihar729@gmail.com>
2021-11-07 10:15:49 +0530

    Ability to genrte the random number

--- src/day_4/SnakeLadder.java
+++ src/day_4/SnakeLadder.java
@@ -6,6 +6,8 @@ public class SnakeLadder {
         System.out.println("Welcome to the Snake and Ladder Game");
         int position=0;
         System.out.println("The position of The player is:"+position);
+        int dieCheck =((int)(Math.random()*10)%6)+1;
+        System.out.println("The random on die is:" +dieCheck);
 
     }
 }

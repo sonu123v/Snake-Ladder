ecc2f3cf4660451679968de708d459f38f50760d
sonu parihar <sparihar729@gmail.com>
2021-11-07 10:35:06 +0530

    uc3 check for the option

--- src/day_4/SnakeLadder.java
+++ src/day_4/SnakeLadder.java
@@ -1,13 +1,31 @@
 package day_4;
 
 public class SnakeLadder {
+
+
+    public static final int NO_Play=0;
+    public static final int LADDER=1;
+    public static final int SNAKE=2;
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Snake and Ladder Game");
         int position=0;
-        System.out.println("The position of The player is:"+position);
         int dieCheck =((int)(Math.random()*10)%6)+1;
-        System.out.println("The random on die is:" +dieCheck);
-
+        int optionCheck=((int)(Math.random()*10)%3);
+        switch (optionCheck)
+        {
+            case NO_Play:
+                position=position;
+                break;
+            case  LADDER:
+                position=position+dieCheck;
+                break;
+            default:
+                position=position-dieCheck;
+                if(position<0){
+                    position=0;
+                }
+        }
+        System.out.println("The position of The player is:"+position);
     }
 }

f9d78e773ebc8abd86fd9457c00bcf02a8e8ce35
sonu parihar <sparihar729@gmail.com>
2021-11-07 10:46:54 +0530

    Winning position

--- src/day_4/SnakeLadder.java
+++ src/day_4/SnakeLadder.java
@@ -1,31 +1,36 @@
 package day_4;
 
 public class SnakeLadder {
+    
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
-
-    public static final int NO_Play=0;
-    public static final int LADDER=1;
-    public static final int SNAKE=2;
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to the Snake and Ladder Game");
-        int position=0;
-        int dieCheck =((int)(Math.random()*10)%6)+1;
-        int optionCheck=((int)(Math.random()*10)%3);
-        switch (optionCheck)
+    public static void main(String[] args)
+    {
+        int position = 0;
+        int dieCheck;
+        int optionCheck;
+        while(position<100)
         {
-            case NO_Play:
-                position=position;
-                break;
-            case  LADDER:
-                position=position+dieCheck;
-                break;
-            default:
-                position=position-dieCheck;
-                if(position<0){
-                    position=0;
-                }
+            dieCheck = ((int) ((Math.random())*10)%6)+1;
+            optionCheck =(int) ((Math.random())*10)%3;
+            switch(optionCheck)
+            {
+                case NO_PLAY:
+                    position=position;
+                    break;
+                case LADDER:
+                    position=position+dieCheck;
+                    break;
+                default:
+                    position=position-dieCheck;
+                    if(position<=0)
+                    {
+                        position=0;
+                    }
+            }
         }
-        System.out.println("The position of The player is:"+position);
+        System.out.println("The Position is: "+position);
     }
 }

0922aec9eca5e035b17cc4b3bcc6e761fc87ed03
sonu parihar <sparihar729@gmail.com>
2021-11-07 10:05:52 +0530

    Ability to add the position

--- src/day_4/SnakeLadder.java
+++ src/day_4/SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Snake and Ladder Game");
+        int position=0;
+        System.out.println("The position of The player is:"+position);
 
     }
 }
